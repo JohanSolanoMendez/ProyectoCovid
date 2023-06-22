0a53adf0a5d4b2b32c62ae5761a7b2a5433b04f8
JohanSolanoMendez <johan.solanom@gmail.com>
2023-06-22 09:52:55 -0600

    cargar paciente caso

--- assets/js/Caso.js
+++ assets/js/Caso.js
@@ -40,3 +40,53 @@ function mostrarModalAgregarSintoma(){
 
 function mostrarModalEditarUsuario(){
   modalEditarUsuario.show();}
+
+function llenarDatos(){
+  var Buscador = document.getElementById('Buscador');
+  var contenidoResultado = document.querySelector('#card-body-boleta');
+
+  var nombre = document.getElementById('nombre');
+  var edad = document.getElementById('edad');
+  var fecha = document.getElementById('fecha');
+  var cedulaP = document.getElementById('cedula');
+  var provincia = document.getElementById('provincia');
+  var fechanacimiento = document.getElementById('fechanacimiento');
+  var email = document.getElementById('email');
+  var celular = document.getElementById('celular');
+  var etapaVida = document.getElementById('etapaVida');
+
+  // Crear una instancia del objeto Date
+  var fechaActual = new Date();
+
+  // Obtener el mes, el día y el año de la fecha actual
+  var mes = fechaActual.getMonth() + 1; // Los meses comienzan desde 0, por lo que se suma 1
+  var dia = fechaActual.getDate();
+  var año = fechaActual.getFullYear();
+
+  // Formatear la fecha en el formato deseado (por ejemplo, MM/DD/YYYY)
+  var fechaFormateada = mes + '/' + dia + '/' + año;
+
+  Buscador.addEventListener('input', function(e) {
+      e.preventDefault();
+    
+     var cedula = document.getElementById('Buscador').value;
+
+     fetch("http://SistemaCovid-19.somee.com/Paciente/" + cedula)
+     .then(response => response.json())
+     .then((json) => {
+
+         console.log(json);
+         nombre.textContent = json.nombreCompleto;
+         edad.textContent = json.edad;
+         fecha.textContent = fechaFormateada;
+         cedulaP.textContent = json.cedula;
+         provincia.textContent = json.provincia;
+         email.textContent = json.email;
+         celular.textContent = json.numeroTelefono;
+         etapaVida.textContent = json.etapaVida;
+     })
+     .catch(error => {
+         console.log('Error:', error);
+     });
+  });
+};
